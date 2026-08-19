bbc9340ee40bec413f6d41c47175e985d39139e7
JUANILLO <martinezjaramillojuandiego@gmail.com>
2026-08-18 22:38:23 -0500

    Se implementa redireccion de entrada y salida (< y >) en sh.c

--- user/sh.c
+++ user/sh.c
@@ -5,6 +5,8 @@
 
 #define MAXARGS 32
 
+// Lee lo que escribe el usuario y lo guarda en buf, imprime $ y si el usuario cerro usa -1 para cerrar
+
 int
 getcmd(char *buf, int nbuf)
 {
@@ -16,12 +18,16 @@ getcmd(char *buf, int nbuf)
   return 0;
 }
 
-// Separa buf en un arreglo de argumentos (argv)
+// Separar el string en palabras convirtiendo los espacios en \0 para poder entender el comando (argv)
+// Ademas detecta los simbolos < y > para redireccion, guardando el nombre
+// del archivo en infile/outfile sin meterlos en argv.
 int
-parseline(char *buf, char **argv)
+parseline(char *buf, char **argv, char **infile, char **outfile)
 {
   int argc = 0;
   char *p = buf;
+  *infile = 0;
+  *outfile = 0;
 
   while(*p){
     // saltar espacios
@@ -29,22 +35,47 @@ parseline(char *buf, char **argv)
       *p++ = 0;
     if(*p == 0)
       break;
+
+    // redireccion de entrada: < archivo
+    if(*p == '<'){
+      *p++ = 0;
+      while(*p == ' ')
+        p++;
+      *infile = p;
+      while(*p && *p != ' ' && *p != '\n')
+        p++;
+      continue;
+    }
+
+    // redireccion de salida: > archivo
+    if(*p == '>'){
+      *p++ = 0;
+      while(*p == ' ')
+        p++;
+      *outfile = p;
+      while(*p && *p != ' ' && *p != '\n')
+        p++;
+      continue;
+    }
+
     argv[argc++] = p;
-    while(*p && *p != ' ' && *p != '\n')
+    while(*p && *p != ' ' && *p != '\n' && *p != '<' && *p != '>')
       p++;
   }
   argv[argc] = 0;
   return argc;
 }
+// Ciclo que se repite hasta que el usuario cierre el programa, lee el comando, lo separa en palabras y ejecuta el comando en un proceso hijo, mientras que el proceso padre espera a que termine el hijo.
 
 int
 main(void)
 {
   static char buf[100];
   char *argv[MAXARGS];
+  char *infile, *outfile;
 
   while(getcmd(buf, sizeof(buf)) >= 0){
-    int argc = parseline(buf, argv);
+    int argc = parseline(buf, argv, &infile, &outfile);
     if(argc == 0)
       continue;
 
@@ -54,7 +85,26 @@ main(void)
 
     int pid = fork();
     if(pid == 0){
-      // proceso hijo: ejecuta el comando
+      // proceso hijo: primero aplica redirecciones, luego ejecuta el comando
+
+      // redireccion de entrada: cierra fd 0 y abre el archivo en su lugar
+      if(infile){
+        close(0);
+        if(open(infile, O_RDONLY) < 0){
+          printf(2, "cannot open %s\n", infile);
+          exit();
+        }
+      }
+
+      // redireccion de salida: cierra fd 1 y abre/crea el archivo en su lugar
+      if(outfile){
+        close(1);
+        if(open(outfile, O_WRONLY | O_CREATE) < 0){
+          printf(2, "cannot open %s\n", outfile);
+          exit();
+        }
+      }
+
       exec(argv[0], argv);
       printf(2, "exec %s failed\n", argv[0]);
       exit();
