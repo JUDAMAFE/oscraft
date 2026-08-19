845f28397d6c6d56dabd461588fd76300909b8c8
JUANILLO <martinezjaramillojuandiego@gmail.com>
2026-08-18 23:22:36 -0500

    Se agregan las tuberias simples (pipe) en sh.c

--- user/sh.c
+++ user/sh.c
@@ -6,7 +6,6 @@
 #define MAXARGS 32
 
 // Lee lo que escribe el usuario y lo guarda en buf, imprime $ y si el usuario cerro usa -1 para cerrar
-
 int
 getcmd(char *buf, int nbuf)
 {
@@ -30,13 +29,11 @@ parseline(char *buf, char **argv, char **infile, char **outfile)
   *outfile = 0;
 
   while(*p){
-    // saltar espacios
     while(*p == ' ' || *p == '\n')
       *p++ = 0;
     if(*p == 0)
       break;
 
-    // redireccion de entrada: < archivo
     if(*p == '<'){
       *p++ = 0;
       while(*p == ' ')
@@ -47,7 +44,6 @@ parseline(char *buf, char **argv, char **infile, char **outfile)
       continue;
     }
 
-    // redireccion de salida: > archivo
     if(*p == '>'){
       *p++ = 0;
       while(*p == ' ')
@@ -65,51 +61,108 @@ parseline(char *buf, char **argv, char **infile, char **outfile)
   argv[argc] = 0;
   return argc;
 }
-// Ciclo que se repite hasta que el usuario cierre el programa, lee el comando, lo separa en palabras y ejecuta el comando en un proceso hijo, mientras que el proceso padre espera a que termine el hijo.
+
+// Ejecuta un comando  en el proceso actual.
+// Se usa dentro de un hijo ya creado con fork(), para comandos sueltos y para cada lado de un pipe.
+void
+runcmd(char *cmdline)
+{
+  char *argv[MAXARGS];
+  char *infile, *outfile;
+  int argc = parseline(cmdline, argv, &infile, &outfile);
+
+  if(argc == 0)
+    exit();
+
+  if(infile){
+    close(0);
+    if(open(infile, O_RDONLY) < 0){
+      printf(2, "cannot open %s\n", infile);
+      exit();
+    }
+  }
+
+  if(outfile){
+    close(1);
+    if(open(outfile, O_WRONLY | O_CREATE) < 0){
+      printf(2, "cannot open %s\n", outfile);
+      exit();
+    }
+  }
+
+  exec(argv[0], argv);
+  printf(2, "exec %s failed\n", argv[0]);
+  exit();
+}
+
+// Busca un '|' en la linea. Si lo encuentra, corta el string ahi mismo
+// lo convierte en \0 y devuelve un puntero a lo que sigue despues.
+// Si no hay pipe, devuelve 0.
+char *
+findpipe(char *cmdline)
+{
+  char *p = cmdline;
+  while(*p){
+    if(*p == '|'){
+      *p = 0;
+      return p + 1;
+    }
+    p++;
+  }
+  return 0;
+}
 
 int
 main(void)
 {
   static char buf[100];
-  char *argv[MAXARGS];
-  char *infile, *outfile;
 
   while(getcmd(buf, sizeof(buf)) >= 0){
-    int argc = parseline(buf, argv, &infile, &outfile);
-    if(argc == 0)
-      continue;
-
-    // comando interno: exit
-    if(strcmp(argv[0], "exit") == 0)
+    // caso especial: exit se revisa ANTES de separar por pipe,
+    // sobre la linea completa tal cual la escribio el usuario.
+    
+    if(strcmp(buf, "exit\n") == 0 || strcmp(buf, "exit") == 0)
       exit();
 
-    int pid = fork();
-    if(pid == 0){
-      // proceso hijo: primero aplica redirecciones, luego ejecuta el comando
+    char *right = findpipe(buf);
 
-      // redireccion de entrada: cierra fd 0 y abre el archivo en su lugar
-      if(infile){
-        close(0);
-        if(open(infile, O_RDONLY) < 0){
-          printf(2, "cannot open %s\n", infile);
-          exit();
-        }
+    if(right == 0){
+      // No hay pipe: comando simple, igual que antes.
+      int pid = fork();
+      if(pid == 0){
+        runcmd(buf);
+      } else {
+        wait();
       }
-
-      // redireccion de salida: cierra fd 1 y abre/crea el archivo en su lugar
-      if(outfile){
-        close(1);
-        if(open(outfile, O_WRONLY | O_CREATE) < 0){
-          printf(2, "cannot open %s\n", outfile);
-          exit();
-        }
-      }
-
-      exec(argv[0], argv);
-      printf(2, "exec %s failed\n", argv[0]);
-      exit();
     } else {
-      // proceso padre: espera al hijo
+      // Hay pipe: buf tiene el comando izquierdo, right el derecho.
+      int fd[2];
+      pipe(fd);
+
+      int pid1 = fork();
+      if(pid1 == 0){
+        // Hijo izquierdo: su salida va al pipe
+        close(1);
+        dup(fd[1]);
+        close(fd[0]);
+        close(fd[1]);
+        runcmd(buf);
+      }
+
+      int pid2 = fork();
+      if(pid2 == 0){
+        // Hijo derecho: su entrada viene del pipe
+        close(0);
+        dup(fd[0]);
+        close(fd[0]);
+        close(fd[1]);
+        runcmd(right);
+      }
+
+      // El padre cierra ambos extremos y espera a los dos hijos
+      close(fd[0]);
+      close(fd[1]);
+      wait();
       wait();
     }
   }
